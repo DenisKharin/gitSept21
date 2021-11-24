be6a302dc202f1ba5d61e6230a7a1c3ba0d2d06b
DenisKharin <den77kharin@gmail.com>
2021-11-24 19:24:27 +0200

    24.11 - Additional JSlesson5 all

--- JS Lesson/Lesson5/Additional/jslesson5.js
+++ JS Lesson/Lesson5/Additional/jslesson5.js
@@ -1,8 +1,41 @@
 // - Дано натуральное число n. Выведите все числа от 1 до n.
+
+// let n = +prompt(`Введiть яке забажаэте натуральне число - N:`);
+//
+// let printNumsInsideForSecondVariable = (n) => {
+//     document.write(`Ви ввели натуральне число - ${n}<br>`);
+//     document.write(`А ось всi числа вiд 1-цi до ${n}:<br>`)
+//     for (let i = 1; i < n; i++) {
+//         document.write(`${i} числo це - ${i};<br>`);
+//     }
+// }
+// printNumsInsideForSecondVariable(n);
+
 // - Даны два целых числа A и В . Выведите все числа от A до B включительно, в
 //   порядке возрастания, если A < B, или в порядке убывания в противном случае.
 
-
+// let variableA = +prompt(`Введiть значення - А:`);
+// let variableB = +prompt(`Введiть значення - B:`);
+// let printNumsInsideForSecondVariable = (variableA, variableB) => {
+//      if (variableA < variableB) {
+//          document.write(`Ви ввели значення А - ${variableA}<br>`);
+//          document.write(`i значення В - ${variableB}<br>`);
+//          document.write(`Мiж ${variableA} i ${variableB} такi числа:<br>`);
+//          for (variableA; variableA < variableB - 1; variableA++) {
+//              document.write(`${variableA + 1}<br>`)
+//          }
+//      } else if (variableA > variableB) {
+//          document.write(`Ви ввели значення А - ${variableA}<br>`);
+//          document.write(`i значення В - ${variableB}<br>`);
+//          document.write(`Мiж ${variableA} i ${variableB} такi числа:<br>`);
+//          for (variableA - 1; variableA > variableB + 1; variableA--) {
+//              document.write(`${variableA - 1}<br>`);
+//          }
+//      } else {
+//          document.write(`Щось пiшло НЕ ТАК !!!`);
+//      }
+// }
+// printNumsInsideForSecondVariable(variableA, variableB);
 
 // - функція Приймає масив та число "i", та міняє місцями об`єкт який
 //   знаходиться в індексі "i" на "i+1"
@@ -12,6 +45,20 @@
 //   foo([9,8,0,4], 1) // ==> [ 9 ,0, 8, 4 ]
 //   foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
 
+// let swapIndexInArrayAndConnotationVariable = (arr, num) => {
+//     document.write(`Були введенi такi даннi:<br>${arr}, ${num}<br>`);
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i === num) {
+//             let variableIndex = arr[i];
+//             arr[i] = arr[i + 1];
+//             arr[i + 1] = variableIndex;
+//         }
+//     }
+//     document.write(`<div>Пiсля замiни мiсцями iндексiв ${num} i ${num + 1} у массивi маэмо такий результат:<br>${arr}</div><br>`);
+// }
+// swapIndexInArrayAndConnotationVariable([9, 8, 0, 4], 0);
+// swapIndexInArrayAndConnotationVariable([9,8,0,4], 1);
+// swapIndexInArrayAndConnotationVariable([9,8,0,4], 2);
 
 // - Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву.
 //   Зберігаючи при цьому порядок не нульових значень.
